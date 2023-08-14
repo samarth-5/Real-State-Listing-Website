e1487c4b937de015e81c01bb5300a78e88b522af
samarth-5 <128695366+samarth-5@users.noreply.github.com>
2023-08-14 18:31:20 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+document.getElementById("login").addEventListener("click",function(){
+    document.querySelector(".popup").style.display="flex";
+});
+document.querySelector(".close").addEventListener("click",function(){
+    document.querySelector(".popup").style.display="none";
+});
+
+document.getElementById("signup").addEventListener("click",function(){
+    document.querySelector(".popups").style.display="flex";
+});
+document.querySelector(".closes").addEventListener("click",function(){
+    document.querySelector(".popups").style.display="none";
+});
+
+document.getElementById("signin").addEventListener("click",function(){
+    document.querySelector(".popup").style.display="flex";
+});
+document.querySelector(".close").addEventListener("click",function(){
+    document.querySelector(".popup").style.display="none";
+});
---
+++ rent.js
@@ -0,0 +1,13 @@
+document.getElementById("login").addEventListener("click",function(){
+    document.querySelector(".popup").style.display="flex";
+});
+document.querySelector(".close").addEventListener("click",function(){
+    document.querySelector(".popup").style.display="none";
+});
+
+document.getElementById("signup").addEventListener("click",function(){
+    document.querySelector(".popups").style.display="flex";
+});
+document.querySelector(".closes").addEventListener("click",function(){
+    document.querySelector(".popups").style.display="none";
+});
